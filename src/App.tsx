67b1c9090d9fe222e07a539334ecd44b345af51e
yongsk0066 <yongsk0066@naver.com>
2021-12-03 23:55:58 +0900

    chrome api test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [url, setUrl] = useState<string | undefined>("");
+
+    useEffect(() => {
+        async function getCurrentTab() {
+            let queryOptions = { active: true, currentWindow: true };
+            let [tab] = await chrome.tabs.query(queryOptions);
+            setUrl(tab?.url);
+        }
+        getCurrentTab();
+    }, []);
+
+    return (
+        <div className="App">
+            d
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    {url}
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
